9ca8ef26a4678088f7803b355db044d0d61f69fb
Than Thanh Duy <thanthanhduy.work@gmail.com>
2021-09-06 00:28:50 +0700

    add Font

--- Client/App.tsx
+++ Client/App.tsx
@@ -2,25 +2,34 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Color from './styles/colors/Color'
+import { useFonts } from 'expo-font'
 //font
 import FontSize from './styles/fonts/FontSize'
+import FontFamily from './styles/fonts/FontFamily'
+
 import SignUpScreen from './screens/Sign Up/index'
 
 export default function App() {
   //load font
+  const [loaded] = useFonts(FontFamily)
+
+  if (!loaded) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <SignUpScreen />
     </View>
-  );
+  )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:  Color.$BackgroundDark,
+    backgroundColor: Color.$BackgroundDark,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   text: {
     fontSize: FontSize.$Headline
--- Client/screens/Sign Up/style.ts
+++ Client/screens/Sign Up/style.ts
@@ -1,39 +1,43 @@
-import { Dimensions, StyleSheet } from "react-native";
-import Color from "../../styles/colors/Color";
-import FontSize from "../../styles/fonts/FontSize";
+import { Dimensions, StyleSheet } from 'react-native'
+import Color from '../../styles/colors/Color'
+import FontSize from '../../styles/fonts/FontSize'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop:44
+    marginTop: 44
   },
   Input: {
     backgroundColor: Color.$Dark,
     borderRadius: 3,
-    width: Dimensions.get("screen").width * 0.9,
-    marginBottom: 8
+    width: Dimensions.get('screen').width * 0.9,
+    marginBottom: 8,
+    fontFamily: 'Poppins-bold'
   },
   Button: {
     backgroundColor: Color.$PrimaryDark,
     borderRadius: 25,
     height: 48,
-    justifyContent: 'center'  
+    justifyContent: 'center',
+    marginTop: 24
   },
-  header:{
+  header: {
+    fontFamily: 'Poppins-bold',
     fontSize: FontSize.$Headline,
-    fontWeight: '700',
-    lineHeight: 34,
+    // fontWeight: '700',
+    lineHeight: 44,
+    color: '#F6F6F6'
+  },
+  body_container: {
+    marginTop: 50,
+    padding: 10
+  },
+  lisence: {
+    fontFamily: 'Poppins-regular',
     color: '#F6F6F6',
-  },
-  body_container:{
-    marginTop:50,
-    padding:10
-  },
-  lisence:{
-    color:'#F6F6F6',
-    textAlign:'center',
-    opacity:10,
+    textAlign: 'center',
+    opacity: 10,
     marginTop: 40,
     fontSize: FontSize.$11px
   }
-});
-export default styles;
+})
+export default styles
